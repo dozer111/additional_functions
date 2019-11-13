c7a2c2aa34b4e19f173c44ba525fef4caa4fe1cc
dozer111 <alkhonko@gmail.com>
2019-11-13 21:27:17 +0200

    first commit)

---
+++ src/variables.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * List of custom functions for variables
+ * This is the addition for https://www.php.net/manual/ru/function.empty.php
+ */
+
+namespace Dozer111\PhpAdditionalFunctions;
+
+if (!function_exists('emptiest')) {
+    /**
+     * Show is the value is really empty
+     * Btw we assume, that $value is exists
+     *
+     * @param $value
+     * @return bool
+     */
+    function emptiest($value): bool
+    {
+        $type = gettype($value);
+
+        switch ($type) {
+            case 'boolean':
+            case 'integer':
+            case 'double':
+            case 'float':
+            case 'object':
+                return false;
+            case 'string':
+                return !boolval(mb_strlen($value)); // 0 => true, 1+ => false
+            case 'array':
+                return empty($input);
+            default:
+                return true;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
